3fa7b8b5a21f80c246f5f263cc1fd373c9591acd
alimdzhatdoev <alimdzhatdoev1@gmail.com>
2023-10-29 10:15:26 +0300

    update

--- config_db.js
+++ config_db.js
@@ -1,53 +1,52 @@
 export function schema() {
     return ({
-            "news" : {
-                "menuName" : "Новости",
-                "title": {
-                    "name" : "Название новости",
-                    "element" : "input",
-                    "type": "text",
-                    "required": true
-                },
-                "text": {
-                    "name" : "Текст новости",
-                    "element" : "textarea",
-                    "type": "text",
-                    "required": true
-                },
-                "img": {
-                    "name" : "Картинка новости",
-                    "element" : "input",
-                    "type": "file",
-                    "required": true
-                },
+        "news": {
+            "menuName": "Новости",
+            "title": {
+                "name": "Название новости",
+                "element": "input",
+                "type": "text",
+                "required": true
             },
-            "events" : {
-                "menuName" : "Мероприятия",
-                "title": {
-                    "name" : "Название мероприятия",
-                    "element" : "input",
-                    "type": "text",
-                    "required": true
-                },
-                "desc": {
-                    "name" : "Описание мероприятия",
-                    "element" : "input",
-                    "type": "text",
-                    "required": true
-                },
-                "text": {
-                    "name" : "Текст мероприятия",
-                    "element" : "textarea",
-                    "type": "text",
-                    "required": true
-                },
-                "img": {
-                    "name" : "Картинка мероприятия",
-                    "element" : "input",
-                    "type": "file",
-                    "required": true
-                },
-            }
+            "text": {
+                "name": "Текст новости",
+                "element": "textarea",
+                "type": "text",
+                "required": true
+            },
+            "img": {
+                "name": "Картинка новости",
+                "element": "input",
+                "type": "file",
+                "required": true
+            },
+        },
+        "events": {
+            "menuName": "Мероприятия",
+            "title": {
+                "name": "Название мероприятия",
+                "element": "input",
+                "type": "text",
+                "required": true
+            },
+            "desc": {
+                "name": "Описание мероприятия",
+                "element": "input",
+                "type": "text",
+                "required": true
+            },
+            "text": {
+                "name": "Текст мероприятия",
+                "element": "textarea",
+                "type": "text",
+                "required": true
+            },
+            "img": {
+                "name": "Картинка мероприятия",
+                "element": "input",
+                "type": "file",
+                "required": true
+            },
         }
-    )
+    })
 }
